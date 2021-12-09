7bc8a510c535d260a65428203ab4725dc084e415
joker <Joker_Ren@163.com>
2021-12-09 23:50:32 +0800

    joker分支

---
+++ oss/src/main/java/com/joker/oss/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.joker.oss.controller;
+
+
+public class TestController {
+
+
+
+
+    public static void main(String[] args) {
+        demo();
+
+        System.out.println("这里是joker分支");
+    }
+
+
+    public static void demo(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("demo");
+//        Object o = null;
+//        o.toString();
+        System.out.println("dddddd");
+    }
+
+}
